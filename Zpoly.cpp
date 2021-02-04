109de651a5f47ea731b81f3ff6fde613fa657585
patrick <pw00242@surrey.ac.uk>
2021-02-04 12:32:59 +0000

    Evaluate seems to work now, need to check evaluateSingleVariable not broken

--- Zpoly.cpp
+++ Zpoly.cpp
@@ -88,25 +88,23 @@ public:
         } else {
             // otherwise we need to do mask-rightshift-xor
             BitVector unmask_shifted = ~(*mask_ptrs[variable_index]) & value;
-            Zpoly mask_shifted = (*this)>>(variable_index);
+            Zpoly mask_shifted = (*this)>>(variable_index+1);
             return Zpoly(mask_shifted.value ^ unmask_shifted);
         }
     }
 
-/*   ------ this function is fubar for unknown reasons ----------
     // evaulate for given set of inputs, given as a bitset
     BitVector evaluate(std::bitset<num_variables> inputs) const {
         // iterate through variables, evaulating for each, final restlt should
         // be either 1 or 0
         Zpoly intermediate_result = *this;
         for (int variable_index=0; variable_index<num_variables; variable_index++) {
-            std::cout << "IR: " << intermediate_result.value << std::endl;
             intermediate_result = intermediate_result.evaluateSingleVariable(variable_index, inputs[variable_index]);
 
         }
         return intermediate_result.value;
     }
-*/
+
 
     // left and right shifts are defined as mask shifts, going left uses inverse
     // of mask (i.e. to be used in multiplication algorithm),
@@ -128,9 +126,10 @@ public:
       if (shift_amt == 0) {
           return *this;
       } else {
-          BitVector mask = *mask_ptrs[shift_amt-1];
+          unsigned int variable_index = shift_amt-1;
+          BitVector mask = *mask_ptrs[variable_index];
           BitVector masked = mask & value;
-          BitVector shifted = masked >> (1<<(shift_amt-1));
+          BitVector shifted = masked >> (1<<(variable_index));
           return Zpoly(shifted);
       }
     }
@@ -166,7 +165,7 @@ int main() {
     for(int i=0; i<(1<<num_bits); i++) {
         Zpoly myp(i);
         std::cout << "Q: " << myp.toString()<< std::endl;
-        std::cout << "A: " << (myp*Zpoly(16)).toString() << std::endl;
+        std::cout << "A: " << Zpoly(myp.evaluate(std::bitset<3>("111"))).toString() << std::endl;
     }
 
     return 0;
